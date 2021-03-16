1c1948f96f470bc7229c30fdcd7f197e834e27e4
Nicolas Rose <nickfbp@gmail.com>
2021-03-16 14:03:53 -0600

    Added Home Component

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <section
+          id="colorlib-hero"
+          className="js-fullheight"
+          data-section="home"
+        >
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{ backgroundImage: "url(images/img_bg.jpg)" }}>
+                <div className="overlay"></div>
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>
+                            Hi! <br />
+                            I'm Nick
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://docs.google.com/document/d/1gQavxKvpLF8HkM62xZQOt8EFggV0sHHfnJsmD7HDyYc/edit?usp=sharing"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Resume
+                              <i className="icon-download4" />
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{ backgroundImage: "url(images/img_bg_2.jpg)" }}>
+                <div className="overlay"></div>
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>
+                            I am <br />a Developer
+                          </h1>
+                          <p>
+                            <a
+                              className="btn btn-primary btn-learn"
+                              href="https://github.com/nico-nikolai"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Github <i className="icon-briefcase3"></i>
+                            </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Home;
