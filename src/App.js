b47eb31ceb15940caaa24d3014472fe373119013
Nicolas Rose <nickfbp@gmail.com>
2021-03-18 10:20:53 -0600

    Added File Structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
+import About from './components/AboutComponent';
 import Home from './components/HomeComponent';
+import Intro from './components/IntroComponent';
+import Projects from './components/ProjectsComponent';
 import Sidebar from './components/SidebarComponent';
-
+import Timeline from './components/TimelineComponent';
 
 function App() {
   return (
     <div id="colorlib-page">
-    <div id="container-wrap">
-    <Sidebar />
-    <div id="colorlib-main">
-      <Home />
-    </div>
-    </div>
+      <div id="container-wrap">
+        <Sidebar />
+        <div id="colorlib-main">
+          <Home />
+        </div>
+      </div>
     </div>
   );
 }
